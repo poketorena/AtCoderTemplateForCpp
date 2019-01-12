31afd97e6aea402f460307c696e0143886636bdc
poketorena <poketorena@users.noreply.github.com>
2019-01-12 11:53:00 +0900

    プロジェクト ファイルを追加します。

---
+++ AtCoderTemplateForCpp/main.cpp
@@ -0,0 +1,11 @@
+#define _USE_MATH_DEFINES // for C++  
+#include "bits/stdc++.h"
+#define NUM ((long long)1000000007)
+#define int unsigned long long
+
+using namespace std;
+
+signed main()
+{
+    cout << "Hello World!" << endl;
+}
